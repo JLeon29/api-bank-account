04db48495c3eb22904175f4926ad79d4e8e69f10
Juan Jose Leon Fretel <jleonfre@emeal.nttdata.com>
2022-05-12 17:38:42 -0500

    Update project

--- api-bank-account/src/test/java/com/nttdata/account/ApiBankAccountApplicationTests.java
+++ api-bank-account/src/test/java/com/nttdata/account/ApiBankAccountApplicationTests.java
@@ -2,6 +2,7 @@ package com.nttdata.account;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 
 @SpringBootTest
 class ApiBankAccountApplicationTests {
